468b5adb9753c68844ae08a4dbcd83699b29dbda
Dileep Bellamkonda <dileep@Dileeps-MacBook-Pro.local>
2024-04-12 00:38:11 +0800

    replace depricated k6 image with grafana/k6
    # Conflicts:
    #	docker-compose.yml
    #	grafana/dashboards/k6Hosted.json

--- readme.md
+++ readme.md
@@ -4,6 +4,5 @@ docker-compose up -d influxdb grafana
 
 ./run.sh
 
-
 view metrics in influx container
 docker exec -it k6-starter-kit-influxdb-1 /usr/bin/influx
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 exit_c=0
 echo "Start the performance test"
 BUILD="$(date | sed -e "s/ /_/g")"
-docker-compose run k6 run /scripts/fav.js --tag pipeline_id=$BUILD || exit_c=0
+docker-compose run --rm k6 run /scripts/fav.js --tag pipeline_id=$BUILD || exit_c=0
 echo "End performance test"
 exit $exit_c
